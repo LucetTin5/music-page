3e025dbfe7ea71880c02fde4a04cf0c37b23d98f
LucetTin5 <lucettin5@gmail.com>
2025-02-11 15:19:08 +0900

    chore: banner mock under mock/

--- src/components/layout/Banner.tsx
+++ src/components/layout/Banner.tsx
@@ -4,52 +4,7 @@ import {
   CarouselIndicator,
   CarouselItem,
 } from '@/components/ui/carousel';
-import { BannerType } from '@/types/banner';
-
-const banners: BannerType[] = [
-  {
-    id: 1,
-    image: 'https://picsum.photos/400/150',
-    link: 'https://www.hanteochart.com/',
-    title: '[배너] 1번 배너, 제목, 제목의 연장선, 제목의 연장선',
-    startDateTime: '2025-01-01 00:00',
-    endDateTime: '2025-12-31 10:15',
-    button: {
-      text: '더보기',
-      link: 'https://www.hanteochart.com/',
-    },
-  },
-  {
-    id: 2,
-    image: 'https://picsum.photos/400/150',
-    link: 'https://google.com',
-    title: '[배너] 2번 배너, 제목',
-    startDateTime: '2025-01-01 00:00',
-    endDateTime: '2025-12-31 10:15',
-  },
-  {
-    id: 3,
-    image: 'https://picsum.photos/400/150',
-    link: 'https://naver.com',
-    title: '[배너] 3번 배너, 제목',
-    startDateTime: '2025-01-01 00:00',
-    endDateTime: '2025-12-31 10:15',
-  },
-  {
-    id: 4,
-    image: 'https://picsum.photos/400/150',
-    title: '[배너] 4번 배너, 제목',
-    startDateTime: '2025-01-01 00:00',
-    endDateTime: '2025-12-31 10:15',
-  },
-  {
-    id: 5,
-    image: 'https://picsum.photos/400/150',
-    title: '[배너] 5번 배너, 제목',
-    startDateTime: '2025-01-01 00:00',
-    endDateTime: '2025-12-31 10:15',
-  },
-];
+import { banners } from '@/mock/banner';
 
 export const Banner = () => {
   return (
