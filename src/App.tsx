3c27eb6c4dca659adae02ee6c3b5f09e0beb0b9e
LucetTin5 <lucettin5@gmail.com>
2025-02-11 12:11:55 +0900

    feat: simple footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
+import { Footer } from '@/components/layout/Footer';
 import { Header } from '@/components/layout/Header';
 import { CategoryProvider } from '@/context/category/CategoryProvider';
 
 export const App = () => {
   return (
-    <div className="container max-w-md mx-auto">
+    <div className="container max-w-md mx-auto relative min-h-screen">
       <CategoryProvider>
         <Header />
-        <div />
+        <Footer />
       </CategoryProvider>
     </div>
   );
---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer className="w-full max-w-md h-12 bg-gray-100 fixed bottom-0">
+      <div className="w-full h-full flex items-center justify-center font-semibold text-sm">
+        <p>Footer</p>
+      </div>
+    </footer>
+  );
+};
